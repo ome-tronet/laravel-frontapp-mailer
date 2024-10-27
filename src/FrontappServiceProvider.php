7147e309bb4546c0987f4547a0e5d50498f8124b
Oliver Merklinghaus <ome@tro.net>
2024-10-27 17:21:16 +0100

    Fixed ServiceProvider

--- src/FrontappServiceProvider.php
+++ src/FrontappServiceProvider.php
@@ -8,15 +8,19 @@ use tronet\FrontappService\Mail\Transports\FrontappTransport;
 
 class FrontappServiceProvider extends ServiceProvider
 {
-    public function register(): void
-    {
-        $this->mergeConfigFrom(
-            __DIR__.'/../config/frontapp-mailer.php', 'mail.mailers'
-        );
-    }
+    public function register(): void {}
 
     public function boot(): void
     {
+        $this->publishes([
+            __DIR__.'/../config/frontapp-mailer.php' => config_path('frontapp-mailer.php'),
+        ]);
+
+        $this->app['config']['mail.mailers'] = array_merge(
+            $this->app['config']['mail.mailers'],
+            $this->app['config']['frontapp-mailer'] ?? []
+        );
+
         Mail::extend('front', function (array $config = []) {
             return new FrontappTransport(new FrontappService($config));
         });
