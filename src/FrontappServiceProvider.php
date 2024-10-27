4091580b3278d7fdb667efe28158f6ba59eb0ea2
Oliver Merklinghaus <ome@tro.net>
2024-10-27 17:14:58 +0100

    Fixed typo

--- src/FrontappServiceProvider.php
+++ src/FrontappServiceProvider.php
@@ -8,14 +8,15 @@ use tronet\FrontappService\Mail\Transports\FrontappTransport;
 
 class FrontappServiceProvider extends ServiceProvider
 {
-    public function register(): void {}
-
-    public function boot(): void
+    public function register(): void
     {
         $this->mergeConfigFrom(
             __DIR__.'/../config/frontapp-mailer.php', 'mail.mailers'
         );
+    }
 
+    public function boot(): void
+    {
         Mail::extend('front', function (array $config = []) {
             return new FrontappTransport(new FrontappService($config));
         });
